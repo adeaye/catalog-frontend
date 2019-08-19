4431aa880474b67dec90fdb9c3023a1aed2b52cd
ade <ade.armyatna.y@gmail.com>
2019-08-19 10:11:19 +0700

    initial commit react-typescript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+class App extends React.Component {
+  public render() {
+    return (
+      <React.Fragment>
+        <h1>Hallo Dunia</h1>
+      </React.Fragment>
+    );
+  }
+
+export default App;
---
+++ src/utils/config.tsx
@@ -0,0 +1,1 @@
+export const BaseApiUrl = process.env.REACT_APP_BASE_API_URL;
