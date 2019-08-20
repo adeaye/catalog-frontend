ed4421c2a856ac2c810e091621488646cf6f0da0
ade <ade.armyatna.y@gmail.com>
2019-08-20 18:19:34 +0700

    add loader on product list page

--- src/container/catalog/CatalogPage.tsx
+++ src/container/catalog/CatalogPage.tsx
@@ -1,15 +1,12 @@
-import * as React from 'react';
-import { fetchProducts } from './CatalogActions';
-import { connect } from 'react-redux';
-import { ProductRequestUriParams } from './CatalogInterfaces';
-import { 
-  Grid, 
-  Container, 
-  // Card 
-} from 'semantic-ui-react';
-// import CatalogCard from './components/CatalogCard';
-import CardWrapper from './components/CardWrapper';
-import { RouteComponentProps } from 'react-router';
+import * as React from "react";
+import { fetchProducts } from "./CatalogActions";
+import { connect } from "react-redux";
+import { ProductRequestUriParams } from "./CatalogInterfaces";
+import { Grid, Container } from "semantic-ui-react";
+import CardWrapper from "./components/CardWrapper";
+import { RouteComponentProps } from "react-router";
+import CardLoader from "src/components/CardLoader";
+import Spacer from "src/components/Spacer";
 
 interface Props extends RouteComponentProps<any> {
   isLoading: boolean;
@@ -26,64 +23,68 @@ class CatalogPage extends React.Component<Props, any> {
     hasNextPage: true,
     isNextPageLoading: false,
     items: [],
-    number: 0,
+    number: 0
   };
-  
+
   async componentDidMount() {
     await this.props.dispatchFetchProducts();
-    console.log('props', this.props);
   }
   loadNextPage = () => {
-    const {page, dispatchFetchProducts} = this.props;
-    dispatchFetchProducts({page: page + 1});
-  }
+    const { page, dispatchFetchProducts } = this.props;
+    dispatchFetchProducts({ page: page + 1 });
+  };
   goToDetailPage = (productId: number) => {
-    const {history} = this.props;
+    const { history } = this.props;
     history.push(`catalog/${productId}`);
-  }
+  };
   render() {
-    // const { hasNextPage, isNextPageLoading, items } = this.state;
-    const {isLoading, page, totalPages, byIds, entities} = this.props;
-    return(
-      // <CardWrapper
-      //         hasNextPage={hasNextPage}
-      //         isNextPageLoading={isNextPageLoading}
-      //         items={items}
-      //         loadNextPage={this._loadNextPage}
-      //       />
-      <Container fluid={true} style={{height: '100%'}}>
-        <Grid style={{height: '100%'}}>
-          {/* <Grid.Row style={{height: '100%'}} centered={true} columns={3}> */}
-            {/* <Grid.Column>
-              <CatalogCard/>
-              <CatalogCard/>
-              <CatalogCard/>
-            </Grid.Column> */}
-            <Grid.Column>
-            <CardWrapper
-              hasNextPage={page < totalPages}
-              isNextPageLoading={isLoading}
-              items={entities}
-              itemIds={byIds}
-              loadNextPage={this.loadNextPage}
-              goToDetail={this.goToDetailPage}
-            />
-            </Grid.Column>
-          {/* </Grid.Row> */}
+    const { isLoading, page, totalPages, byIds, entities } = this.props;
+    return (
+      <Container fluid={true} style={{ height: "100%" }}>
+        <Grid style={{ height: "100%" }}>
+          <Grid.Column>
+            {entities ? (
+              <CardWrapper
+                hasNextPage={page < totalPages}
+                isNextPageLoading={isLoading}
+                items={entities}
+                itemIds={byIds}
+                loadNextPage={this.loadNextPage}
+                goToDetail={this.goToDetailPage}
+              />
+            ) : (
+              <React.Fragment>
+                <Spacer height={65} />
+                <CardLoader style={{ width: "450px", margin: "auto" }} />
+                <Spacer height={20} />
+                <CardLoader style={{ width: "450px", margin: "auto" }} />
+              </React.Fragment>
+            )}
+          </Grid.Column>
         </Grid>
       </Container>
     );
   }
 }
 
-const MSTP = (state) => {
-  const {isLoading, page, byIds, entities, itemsPerPage, totalPages} = state.catalog;
+const MSTP = state => {
+  const {
+    isLoading,
+    page,
+    byIds,
+    entities,
+    itemsPerPage,
+    totalPages
+  } = state.catalog;
 
-  return {isLoading, page, byIds, entities, itemsPerPage, totalPages};
+  return { isLoading, page, byIds, entities, itemsPerPage, totalPages };
 };
 
 const MDTP = {
   dispatchFetchProducts: fetchProducts
 };
 
-export default connect(MSTP, MDTP)(CatalogPage);
+export default connect(
+  MSTP,
+  MDTP
+)(CatalogPage);
